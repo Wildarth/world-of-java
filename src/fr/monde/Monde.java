1177e8cb4551227dbad85d3e7c7b76ff81ad180f
Guillaume Jovelin <jovelinguillaume@gmail.com>
2021-01-18 11:44:21 +0100

    Exercice 4.1: Ajout de monstreFactory().

--- src/fr/monde/Monde.java
+++ src/fr/monde/Monde.java
@@ -2,14 +2,22 @@ package fr.monde;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
+import fr.personnage.Monstre;
 import fr.personnage.Personnage;
 import fr.utilitaire.Tool;
 
 public class Monde {
+	
+	public static String[] debutNom = new String[] {"chien", "loup", "bandit", "magicien"};
+	public static String[] finNom = new String[] {"mechant", "de feu", "des bois", "des ténèbres"};
 
 	private static List<Personnage> personnages = new ArrayList<>();
+	private static List<Monstre> monstres = new ArrayList<>();
 
+	private static Random random = new Random();
+	
 	private Monde() {
 
 	}
@@ -30,6 +38,23 @@ public class Monde {
 		personnages.add(personnage);
 
 		return personnage;
+	}
+	
+	/**
+	 * Créer un monstre avec tous ses attributs. Son nom et ses attributs son générés aléatoirement.
+	 * Le monstre est ajouté à la liste des personnages du monde.
+	 * 
+	 * @return une instance de la classe Monstre correctement instancié.
+	 */
+	public static Monstre monstreFactory() {
+
+		String nom = debutNom[random.nextInt(debutNom.length)] + finNom[random.nextInt(finNom.length)];
+
+		Monstre monstre = new Monstre(nom, random.nextInt(65) + 10, random.nextInt(10) + 5);
+
+		monstres.add(monstre);
+
+		return monstre;
 	}
 
 	/**
