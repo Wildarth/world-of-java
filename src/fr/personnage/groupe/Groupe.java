e3fac6a3320bc75766a82f9f1a13394ac2fda3f5
Guillaume Jovelin <jovelinguillaume@gmail.com>
2021-01-18 16:41:52 +0100

    Exercice 9.3.3 : Surchage des méthodes attaquer() et defendre() de la classe Groupe.

--- src/fr/personnage/groupe/Groupe.java
+++ src/fr/personnage/groupe/Groupe.java
@@ -2,6 +2,7 @@ package fr.personnage.groupe;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import fr.personnage.Combattant;
 
@@ -11,6 +12,8 @@ public class Groupe implements Combattant {
 	private String nom;
 	private int degat;
 	private int pointDeVie;
+	
+	private Random random = new Random();
 
 	@Override
 	public String getNom() {
@@ -62,16 +65,20 @@ public class Groupe implements Combattant {
 		return estMort;
 	}
 
+	/**
+	 * Attaque avec un combattant aléatoire du groupe.
+	 */
 	@Override
 	public void attaquer(Combattant adversaire) {
-		// TODO Auto-generated method stub
-		
+		combattants.get(random.nextInt(combattants.size())).attaquer(adversaire);
 	}
 
+	/**
+	 * Un combattant aléatoire du groupe encaisse le coup.
+	 */
 	@Override
 	public int defendre(int degat) {
-		// TODO Auto-generated method stub
-		return 0;
+		return combattants.get(random.nextInt(combattants.size())).defendre(degat);
 	}
 	
 }
